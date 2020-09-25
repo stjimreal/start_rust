82e4407a8a2e07779d4a5099d853b6f7167966f6
刘立京 <stjimreal@gmail.com>
2020-09-25 17:05:51 +0800

    ac

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
-fn main() {
+const MAX_POINTS: u32 = 100000;
+use std::io;
+fn prt() {
     let a = 12;
-    println!("a is {0} {0} a is {0}", a);
+    println!("a is {0} {0} a is {0} {1}", a, MAX_POINTS);
+}
+
+fn main(){
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("You guessed: {}\n", guess);
 }
